a13be5ef29cb7562b8fdab3c549f1b19e16780b2
Drew Bornt <andrew.bornt@gmail.com>
2020-10-30 10:03:16 -0500

    added connection to mongo atlas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {ApolloServer} = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
 
 
 const typeDefs = gql`
@@ -20,6 +23,15 @@ const server =  new ApolloServer({
     resolvers
 });
 
-server.listen({port: 5000}).then((res) => {
-    console.log(`console running at ${res.url}`);
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
+    .then(()=> {
+        console.log('MongoDB connected');
+        return server.listen({ port: 5000 });
+})
+    .then((res) => {
+        console.log(`console running at ${res.url}`);
 });
+
+
+
